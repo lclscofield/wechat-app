6e8a910efd339410a546c9980692518e5ab19d62
lclscofield <lclscofield@foxmail.com>
2019-04-28 15:25:40 +0800

    实现图片上拉及懒加载，优化显示效果

--- tuchong/pages/index/index.js
+++ tuchong/pages/index/index.js
@@ -9,30 +9,79 @@ Page({
     post_id: '',
     type: 'refresh'
   },
-  onLoad: function() {
+  // 页面加载
+  onLoad() {
+    wx.showLoading({
+      title: '加载中...',
+      mask: true
+    })
+    this.getFeed().then(res => {
+      this.setData({
+        feedList: res.feedList
+      })
+      wx.hideLoading()
+      console.log(this.data.feedList)
+    })
+  },
+  // 下拉刷新
+  onPullDownRefresh() {
+    this.setData({
+      page: 1,
+      post_id: '',
+      type: 'refresh'
+    })
     this.getFeed().then(res => {
       this.setData({
         feedList: res.feedList
       })
       console.log(this.data.feedList)
+      wx.stopPullDownRefresh()
     })
   },
-  getFeed: function() {
+  // 上拉触底
+  onReachBottom() {
+    const len = this.data.feedList.length
+    const post_id = this.data.feedList[len - 1].post_id || ''
+    this.setData({
+      page: this.data.page + 1,
+      post_id,
+      type: 'loadmore'
+    })
+    this.getFeed().then(res => {
+      this.setData({
+        feedList: this.data.feedList.concat(res.feedList)
+      })
+      console.log(this.data.feedList)
+    })
+  },
+  loadImg(e) {
+    const idx = e.target.dataset.index
+    const hidden = `feedList[${idx}].hidden`
+    this.setData({
+      [hidden]: false
+    })
+  },
+  // 获取图片数据
+  getFeed() {
     return new Promise((resolve, reject) => {
       const { page, post_id, type } = this.data
+      let data = {
+        page,
+        type
+      }
+      if (post_id) {
+        data.post_id = post_id
+      }
       wx.request({
         url: 'https://api.tuchong.com/feed-app',
-        data: {
-          page,
-          post_id,
-          type
-        },
+        data,
         success: res => {
           let data = res.data
           if (data.result === 'SUCCESS') {
             res.data.feedList.forEach(item => {
               if (item.images[0]) {
                 item.imgUrl = `https://photo.tuchong.com/${item.images[0].user_id}/f/${item.images[0].img_id}.jpg`
+                item.hidden = true
               }
             })
             resolve(res.data)
