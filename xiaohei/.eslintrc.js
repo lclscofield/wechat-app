6e1a655e3900949c72aea0751eeb85589aa74411
lclscofield <lclscofield@foxmail.com>
2019-06-03 17:25:47 +0800

    xiaohei wepy => mpvue

--- xiaohei/.eslintrc.js
+++ xiaohei/.eslintrc.js
@@ -1,22 +1,22 @@
+// http://eslint.org/docs/user-guide/configuring
+
 module.exports = {
   root: true,
-  globals: { wx: true },
   parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module'
   },
   env: {
-    browser: true
+    browser: false,
+    node: true,
+    es6: true
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
   extends: 'standard',
-  // required to lint *.wpy files
+  // required to lint *.vue files
   plugins: [
     'html'
   ],
-  settings: {
-    'html/html-extensions': ['.html', '.wpy']
-  },
   // add your custom rules here
   'rules': {
     // allow paren-less arrow functions
@@ -24,7 +24,19 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'space-before-function-paren': 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  },
+  globals: {
+    App: true,
+    Page: true,
+    wx: true,
+    swan: true,
+    tt: true,
+    my: true,
+    getApp: true,
+    getPage: true,
+    requirePlugin: true,
+    mpvue: true,
+    mpvuePlatform: true
   }
 }
---
+++ xiaohei/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  created () {
+    /*
+     * 平台 api 差异的处理方式:  api 方法统一挂载到 mpvue 名称空间, 平台判断通过 mpvuePlatform 特征字符串
+     * 微信：mpvue === wx, mpvuePlatform === 'wx'
+     * 头条：mpvue === tt, mpvuePlatform === 'tt'
+     * 百度：mpvue === swan, mpvuePlatform === 'swan'
+     * 支付宝(蚂蚁)：mpvue === my, mpvuePlatform === 'my'
+     */
+  }
+}
+</script>
+
+<style>
+</style>
