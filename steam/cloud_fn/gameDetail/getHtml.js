6d325086f51c82d10f72c48640ffd564f72a7c11
lclscofield <lclscofield@foxmail.com>
2019-07-12 15:07:34 +0800

    steam 绕过 steam 年龄限制

--- steam/cloud_fn/gameDetail/getHtml.js
+++ steam/cloud_fn/gameDetail/getHtml.js
@@ -2,7 +2,11 @@ const request = require('request')
 
 async function getHtml (options) {
   return new Promise((resolve, reject) => {
-    request(options, (err, res, body) => {
+    // 写入 cookie 绕过年龄限制
+    options.headers = {
+      Cookie: 'birthtime=' + 817804801
+    }
+    request(options, async (err, res, body) => {
       if (!err && res.statusCode === 200) {
         resolve(body)
       } else {
